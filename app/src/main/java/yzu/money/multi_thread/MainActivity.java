cc585cd4bf837b610f503209b0e4a20142257ab4
howdyhi <money@20-17.wlan.yzu.edu.tw>
2016-11-17 14:37:45 +0800

    multi threads

--- app/src/main/java/yzu/money/multi_thread/MainActivity.java
+++ app/src/main/java/yzu/money/multi_thread/MainActivity.java
@@ -20,7 +20,45 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 System.out.println("test");
                 //Log.i("money","test");
+                //way1
+                /*new Thread(){
+                    public void run()
+                    {
+                        int i;
+                        for(i=0;i<100;i++)
+                            System.out.println(i);
+                    }
+                }.start();*/
+
+                //way2
+                mythread t1=new mythread("a",1);
+                mythread t2=new mythread("b",2);
+                mythread t3=new mythread("c",3);
+                mythread t4=new mythread("d",4);
+                mythread t5=new mythread("e",5);
+                t1.start();
+                t2.start();
+                t3.start();
+                t4.start();
+                t5.start();
             }
         });
+    }
+    class mythread extends Thread{
+        int id;
+
+        public mythread(String name, int id) {//constructor
+            super(name);
+            this.id = id;
+
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            int i;
+            for(i=0;i<100;i++)
+                System.out.println("id: "+id+", i: "+i);
+        }
     }
 }
